e833f7cd939cd7a3fb405fed24a34603c5549485
Ivan-Fudko <79043864+Ivan-Fudko@users.noreply.github.com>
2022-02-14 20:13:50 +0500

    3

--- map/main._map.py
+++ map/main._map.py
@@ -24,11 +24,12 @@ class MyMap(QWidget):
         self.map_request = ['http://static-maps.yandex.ru/1.x/?ll=', self.x, ',', self.y,
                             '&spn=', self.masht, ',', self.masht, '&l=map']
         self.setImageToPixmap()
+        self.setSelfFocus()
 
     def getImage(self):
-        self.x = self.x_edit.text().strip()
-        self.y = self.y_edit.text().strip()
-        self.masht = self.mashtab.text().strip()
+        self.x = self.x_edit.text()
+        self.y = self.y_edit.text()
+        self.masht = self.mashtab.text()
         self.map_request = ''.join(['http://static-maps.yandex.ru/1.x/?ll=', self.x, ',',
                                     self.y, '&spn=', self.masht, ',', self.masht, '&l=map'])
         response = requests.get(self.map_request)
@@ -43,8 +44,10 @@ class MyMap(QWidget):
         if is_all_secc:
             self.pixmap = QPixmap(self.map_file)
             self.image.setPixmap(self.pixmap)
+        self.setSelfFocus()
 
     def keyPressEvent(self, event):
+        a = event
         if event.key() == Qt.Key_PageUp:
             try:
                 self.mashtab.setText(str(float(self.mashtab.text()) * 2))
@@ -57,6 +60,35 @@ class MyMap(QWidget):
                 self.setImageToPixmap()
             except FloatingPointError as e:
                 print(e)
+        elif event.key() == Qt.Key_Up:
+            print(2)
+            try:
+                self.y_edit.setText(str(float(self.y_edit.text()) - 2 * float(self.mashtab.text())))
+                self.setImageToPixmap()
+            except FloatingPointError as e:
+                print(e)
+        elif event.key() == Qt.Key_Down:
+            print(1)
+            try:
+                self.y_edit.setText(str(float(self.y_edit.text()) + 2 * float(self.mashtab.text())))
+                self.setImageToPixmap()
+            except FloatingPointError as e:
+                print(e)
+        elif event.key() == Qt.Key_Right:
+            try:
+                self.x_edit.setText(str(float(self.x_edit.text()) + 2 * float(self.mashtab.text())))
+                self.setImageToPixmap()
+            except FloatingPointError as e:
+                print(e)
+        elif event.key() == Qt.Key_Left:
+            try:
+                self.x_edit.setText(str(float(self.x_edit.text()) - 2 * float(self.mashtab.text())))
+                self.setImageToPixmap()
+            except FloatingPointError as e:
+                print(e)
+
+    def setSelfFocus(self):
+        self.setFocusPolicy(Qt.StrongFocus)
 
     def closeEvent(self, event):
         os.remove(self.map_file)
